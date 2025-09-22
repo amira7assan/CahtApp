1f570b75fcd3731e1902b29525e96cf7c7823c9c
amira <amira7assan4444@gmail.com>
2025-09-22 12:51:06 +0200

    ChatCubit

---
+++ lib/screens/cubits/ChatCubit/chat_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+import '../../../constants.dart';
+
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  ChatCubit() : super(ChatInitial());
+  CollectionReference messages = FirebaseFirestore.instance.collection(kMessageCollection);
+  void sendMsg =({required String message ,required String email }){
+    messages.add({
+      kMessage :message,
+      kcreatedAt : DateTime.now(),
+      'id' : email,
+    });
+  };
+}
